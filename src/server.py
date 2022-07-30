0c01e7fd36d23bbf03f085cc11924fb1437b2a85
Nave Itay <59213562+NaveItay@users.noreply.github.com>
2022-07-30 17:09:50 +0300

    Add files via upload

---
+++ src/server.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/clean")
+def clean(number: int):
+    interactions_number = 0
+    answer = "Answer: "
+
+    while number != 1:
+        interactions_number += 1
+        number = collatz_conjecture(number)
+        answer += f'{number},'
+
+    answer += f'interactions number: {interactions_number}'
+    return answer
+
+
+def collatz_conjecture(number):
+    if number % 2 == 0:
+        return number // 2
+    elif number % 2 == 1:
+        return number * 3 + 1
